50c0cc2f617c9dfaf67101b79e1060845358365b
Nikhil-tech2002 <waghnikhil100@gmail.com>
2026-01-11 23:34:22 +0530

    Update Fraud Detection .py

--- Fraud Detection .py
+++ Fraud Detection .py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[40]:
 
 
 import pandas as pd
@@ -15,34 +11,19 @@ import matplotlib.pyplot as plt
 
 
 
-# In[27]:
-
-
 df = pd.read_csv("fraudTrain.csv")   
-
-
-# In[28]:
 
 
 df = df[df['amt'] > 50].reset_index(drop=True)
 
-
-# In[29]:
 
 
 df['trans_date_trans_time'] = pd.to_datetime(df['trans_date_trans_time'])
 df['hour'] = df['trans_date_trans_time'].dt.hour
 
 
-# In[30]:
-
-
 df['dob'] = pd.to_datetime(df['dob'])
 df['age'] = (df['trans_date_trans_time'] - df['dob']).dt.days // 365
-
-
-# In[31]:
-
 
 df.drop(columns=[
     'cc_num', 'first', 'last', 'street', 'city',
@@ -50,8 +31,6 @@ df.drop(columns=[
     'dob', 'trans_date_trans_time'
 ], inplace=True)
 
-
-# In[32]:
 
 
 num_cols = df.select_dtypes(include='number').columns
@@ -61,25 +40,15 @@ cat_cols = df.select_dtypes(include='object').columns
 df[cat_cols] = df[cat_cols].fillna('Unknown')
 
 
-# In[33]:
-
 
 for col in ['category', 'gender', 'job']:
     le = LabelEncoder()
     df[col] = le.fit_transform(df[col])
 
-
-# In[34]:
-
-
 TARGET = 'is_fraud'
 X = df.drop(TARGET, axis=1)
 y = df[TARGET]
 
-
-# In[36]:
-
-
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
@@ -87,25 +56,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
-
-
-# In[37]:
-
 
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-
-# In[38]:
-
-
 model = LogisticRegression(class_weight='balanced', max_iter=1000)
 model.fit(X_train, y_train)
-
-
-# In[39]:
-
 
 y_pred = model.predict(X_test)
 y_prob = model.predict_proba(X_test)[:, 1]
@@ -114,8 +71,18 @@ print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 print("ROC-AUC:", roc_auc_score(y_test, y_prob))
 
+#[[118570   5126]
+ [    82   1098]]
+              precision    recall  f1-score   support
 
-# In[41]:
+           0       1.00      0.96      0.98    123696
+           1       0.18      0.93      0.30      1180
+
+    accuracy                           0.96    124876
+   macro avg       0.59      0.94      0.64    124876
+weighted avg       0.99      0.96      0.97    124876
+
+ROC-AUC: 0.9774255405269122
 
 
 plt.figure(figsize=(6,4))
@@ -124,4 +91,6 @@ plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.title("Transaction Locations (Fraud Highlighted)")
 plt.show()
+
+
 
